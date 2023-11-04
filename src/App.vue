941810e9690a26c0895e9a05c14efe6ff5ea36fa
Depras Nur Yadi <deprasny@gmail.com>
2023-11-04 12:37:08 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <Button
+      label="Primary Button"
+      variant="primary"
+      @click="handleClick"
+    />
+    <Button
+      label="Danger Button"
+      variant="danger"
+      @click="handleClick"
+    />
+  </div>
+</template>
+
+<script>
+import Button from "./components/ui/Button.vue";
+
+export default {
+  components: {
+    Button
+  },
+  methods: {
+    handleClick() {
+      // Handle button click here
+    }
+  }
+};
+</script>
