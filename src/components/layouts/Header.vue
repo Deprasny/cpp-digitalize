0ae49cfce853743faaa7818a3d4538c17b14a22b
mrayandika15 <mrayandika15@gmail.com>
2023-11-13 19:51:09 +0700

    fix(search): searchbar, placeholder

--- src/components/layouts/Header.vue
+++ src/components/layouts/Header.vue
@@ -3,8 +3,8 @@
         <div>
             <input
                 type="text"
-                placeholder="kadal"
-                class="w-[400px] px-4 py-2 text-black placeholder-opacity-50 rounded-lg bg-transparent shadow-lg outline-none appearance-none focus:placeholder-opacity-100"
+                placeholder="Search"
+                class="w-[400px] px-4 py-2 text-black placeholder-opacity-50 rounded-lg bg-transparent shadow-lg outline-none appearance-none focus:placeholder-opacity-100 bg-white"
             />
         </div>
 
