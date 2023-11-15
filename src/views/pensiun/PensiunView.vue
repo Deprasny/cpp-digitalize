de05170fc53470b2d2fe1ff6fc2075a71a956701
deprasny <deprasny@gmail.com>
2023-11-15 23:10:19 +0700

    Add table and buttons to PensiunView.vue

--- src/views/pensiun/PensiunView.vue
+++ src/views/pensiun/PensiunView.vue
@@ -1,5 +1,67 @@
 <template>
-    <div class="flex items-center justify-center min-h-screen">
-        <h1>Pensiun Page</h1>
+    <div class="flex flex-col items-center justify-center">
+        <div class="flex items-center justify-between w-full my-5">
+            <UIButton :icon="IconChevronLeft" :variant="'secondary'"
+                >Evaluasi
+            </UIButton>
+            <router-link to="/mutasi/create">
+                <UIButton :icon="IconPlus">Buat form evaluasi baru </UIButton>
+            </router-link>
+        </div>
+        <Table :columns="columns" :data="data" />
     </div>
 </template>
+
+<script setup>
+import Table from "@/components/BasicTable.vue";
+import { createColumnHelper } from "@tanstack/table-core";
+import { ref } from "vue";
+import UIButton from "@/components/ui/UIButton.vue";
+import IconPlus from "@/components/icons/IconPlus.vue";
+import IconChevronLeft from "@/components/icons/IconChevronLeft.vue";
+
+const makeData = (count) => {
+    const data = [];
+    for (let i = 0; i < count; i++) {
+        data.push({
+            mutasi: `000${i}/MUTASI/INDIVIDU/X/2023`,
+            nama: `User ${i}`,
+            tanggal: new Date().toISOString(),
+            status: "Approved",
+            jenis: "Individu",
+            date: new Date().toISOString(),
+        });
+    }
+    return data;
+};
+const data = ref(makeData(4));
+
+const columnHelper = createColumnHelper();
+const columns = [
+    columnHelper.accessor((row) => row.mutasi, {
+        id: "mutasi",
+        cell: (info) => info.getValue(),
+        header: () => "No Pensiun",
+    }),
+    columnHelper.accessor((row) => row.nama, {
+        id: "nama",
+        cell: (info) => info.getValue(),
+        header: () => "Nama Karyawan",
+    }),
+    columnHelper.accessor((row) => row.tanggal, {
+        id: "tanggal",
+        cell: (info) => info.getValue(),
+        header: () => "Tanggal Pengajuan",
+    }),
+    columnHelper.accessor((row) => row.status, {
+        id: "status",
+        cell: (info) => info.getValue(),
+        header: () => "Status",
+    }),
+    columnHelper.accessor((row) => row.date, {
+        id: "date",
+        cell: (info) => info.getValue(),
+        header: () => "Due Date",
+    }),
+];
+</script>
