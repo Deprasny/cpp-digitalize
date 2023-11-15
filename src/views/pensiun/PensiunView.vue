f7da844204fb8344b92bae7a9781073029c4b7eb
deprasny <deprasny@gmail.com>
2023-11-15 23:30:35 +0700

    Update PensiunView.vue with new button label

--- src/views/pensiun/PensiunView.vue
+++ src/views/pensiun/PensiunView.vue
@@ -2,7 +2,7 @@
     <div class="flex flex-col items-center justify-center">
         <div class="flex items-center justify-between w-full my-5">
             <UIButton :icon="IconChevronLeft" :variant="'secondary'"
-                >Evaluasi
+                >Pensiun
             </UIButton>
             <router-link to="/mutasi/create">
                 <UIButton :icon="IconPlus">Buat form evaluasi baru </UIButton>
