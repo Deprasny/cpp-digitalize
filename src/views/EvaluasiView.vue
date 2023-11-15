09e933165ec8de949e86d063c6fe2070e365e042
Depras Nur Yadi <deprasny@gmail.com>
2023-11-15 15:32:19 +0700

    Delete unused views

--- src/views/EvaluasiView.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="flex items-center justify-center min-h-screen">
-        <h1>Evaluasi Page</h1>
-    </div>
-</template>
--- src/views/PensiunView.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="flex items-center justify-center min-h-screen">
-        <h1>Pensiun Page</h1>
-    </div>
-</template>
