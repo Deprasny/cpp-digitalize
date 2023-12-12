e6b7d5dbe07ea62a2132835b53bf37a7601a57a7
mrayandika15 <mrayandika15@gmail.com>
2023-12-12 13:06:43 +0700

    Update API base URL

--- src/utils/apiServices.js
+++ src/utils/apiServices.js
@@ -1,7 +1,7 @@
 // api.js
 import axios from "axios";
 
-const BASE_URL = process.env.VUE_APP_API_BASE_URL;
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const apiService = axios.create({
     baseURL: BASE_URL,
