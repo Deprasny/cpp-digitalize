76886d700b7a65c4b894a10e7a19df9bd6f35729
mrayandika15 <mrayandika15@gmail.com>
2023-12-18 12:40:47 +0700

    Refactor allowedDates to include a range of years

--- src/views/mutasi/MutasiCreateGroupView.vue
+++ src/views/mutasi/MutasiCreateGroupView.vue
@@ -320,13 +320,16 @@ const removeName = (index) => {
 };
 
 const allowedDates = computed(() => {
-    const currentDate = new Date();
-    const currentYear = currentDate.getFullYear();
+    const startYear = 2022; // You can modify this to the desired start year
+    const endYear = new Date().getFullYear() + 5; // You can modify this to set the number of years into the future
+
     const monthsArray = [];
 
-    for (let month = 0; month < 12; month++) {
-        const firstDayOfMonth = new Date(currentYear, month, 1);
-        monthsArray.push(firstDayOfMonth);
+    for (let year = startYear; year <= endYear; year++) {
+        for (let month = 0; month < 12; month++) {
+            const firstDayOfMonth = new Date(year, month, 1);
+            monthsArray.push(firstDayOfMonth);
+        }
     }
 
     return monthsArray;
