f6f2acd2a4d3b7ff8c6bad3cb7396b86c3baa5b6
mrayandika15 <mrayandika15@gmail.com>
2024-01-08 14:16:32 +0700

    Refactor font styles and update page title

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <!-- App.vue -->
 <template>
-    <router-view></router-view>
+  <router-view></router-view>
 </template>
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&family=Roboto:wght@300;400;500&display=swap");
 * {
-    font-family: "Poppins", sans-serif;
+  font-family: "Poppins", sans-serif;
 }
 </style>
 
+<!-- testing -->
+
 <script>
 export default {
-    mounted() {
-        // Change the page title when the component is mounted
-        document.title = "HR FORM";
-    },
+  mounted() {
+    // Change the page title when the component is mounted
+    document.title = "HR FORM";
+  },
 };
 </script>
